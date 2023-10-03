6110e23a6f1e9d9d72ea7aab9c809713a8128b1f
Dior Diop <122567381+DiorDiop@users.noreply.github.com>
2023-10-03 19:39:15 +0000

    Web Scraping 02_Update

--- Web Scraping 02.py
+++ Web Scraping 02.py
@@ -17,41 +17,106 @@ import pandas as pd
 
 # Create empty lists to stores data later
 job_title = []
-job_name = []
 job_poster = []
 job_pricing = []
 job_location = []
 
 
-# In[4]:
+# In[3]:
 
 
-# get url
+# Get the title
 html = urlopen("https://www.guru.com/d/freelancers/skill/Data-science/")
 bso = soup(html.read())
-for link in bso.find_all('h2'):
-    print(link.a)
+for link in bso.find_all('h2', {'class' : 'serviceListing__title serviceListing__title--dark'}):
+    print(link.a.text)
 
 
-# In[4]:
+# In[17]:
 
 
-# get the html code
-soup = soup(page.text, 'html')
-soup
+# Get Job pricing
+for job in bso.find_all('p', {'class' : 'serviceListing__rates'}):
+    print(job.text.strip())
 
 
 # In[5]:
 
 
-title = soup.find_all('h2')
-title
+#  Get Freelancer name
+for job in bso.find_all('h3', {'class' : 'freelancerAvatar__screenName'}):
+    print(job.a.text)
 
 
-# In[ ]:
+# In[18]:
 
 
+# Get freelancer location and revenue per year
+for job in bso.find_all('p', {'class' : 'freelancerAvatar__meta'}):
+    print(job.span.text)
+    if span is not None:
+        print(span.text.strip())
+    else:
+        print(" ")
 
+
+# In[7]:
+
+
+# Get freelancer description
+for job in bso.find_all('p', {'class' : 'serviceListing__desc'}):
+    print(job.text)
+
+
+# In[8]:
+
+
+# Get freelancer skills
+for job in bso.find_all('div', {'class' : 'skillsList'}):
+    print(job.a.text)
+
+
+# In[9]:
+
+
+for job in bso.find_all('a', {'class' : 'skillsList__skill skillsList__skill--hasHover'}):
+    print(job.text)
+
+
+# In[10]:
+
+
+def freelance(webpage):
+    crawl = urlopen(webpage)
+    bsobj = soup(crawl.read())
+    bsobj_title = bsobj.find_all('h2', {'class' : 'serviceListing__title serviceListing__title--dark'})
+    bsobj_pricing = bsobj.find_all('p', {'class' : 'serviceListing__rates'})
+    bsobj_name = bsobj.find_all('h3', {'class' : 'freelancerAvatar__screenName'})
+    bsobj_location = bsobj.find_all('p', {'class' : 'freelancerAvatar__meta'})
+    for x in range(len(bsobj_title)):
+        job_titlt.append(bsobj_title[x].a.text)
+        job_pricing.append(bsobj_pricing[x].text)
+        job_poster.append(bsobj_name[x].a.text)
+        job_location.append(bsobj_location[x].span.text)
+
+        
+webpage = 'https://www.guru.com/d/freelancers/skill/Data-science/'
+page = 0
+for i in range(1, 1752):
+    page = page + 1
+    webpage = webpage + str(page)
+    freelance(webpage)
+
+
+# In[16]:
+
+
+for job in bso.find_all('p', {'class': 'freelancerAvatar__meta'}):
+    span = job.find('span')  # Look for the <span> tag within the <p> element
+    if span is not None:
+        print(span.text.strip())
+    else:
+        print(" ")
 
 
 # In[ ]:
